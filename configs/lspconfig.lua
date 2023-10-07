0256f42a4d00b4c705c13d3c981b28bfd2e4cfd9
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2023-10-07 17:53:57 -0400

    Replaced pyright with pylsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -9,7 +9,7 @@ local servers = {
     'ltex',
     'bashls',
     'gopls',
-    'pyright',
+    'pylsp',
     'jdtls',
     'html',
     'cssls',
--- configs/mason.lua
+++ configs/mason.lua
@@ -24,6 +24,7 @@ M = {
         'gopls',
         'ocaml-lsp',
         'taplo',
+        'python-lsp-server',
     },
 }
 
