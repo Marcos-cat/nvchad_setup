b673c71e8f961941701de94102384ccf41df1549
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2023-10-30 17:04:32 -0400

    Changed the icon color for text files

--- configs/devicons.lua
+++ configs/devicons.lua
@@ -1,13 +1,24 @@
 local M = {}
 
+local c = {
+    white = '#cdd6f4',
+    yellow = '#f9e2af',
+}
+
 M = {
     color_icons = true,
     strict = true,
+
     override_by_extension = {
         ['ml'] = {
             icon = '',
-            color = '#f9e2af',
+            color = c.yellow,
             name = 'Ocaml',
+        },
+        ['txt'] = {
+            icon = '󰈙',
+            color = c.white,
+            name = 'Text',
         },
     },
 }
