641384d3b4393af9b760f3c64c5c1e3599fb4943
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2023-10-30 19:56:58 -0400

    Added more custom devicons

--- configs/devicons.lua
+++ configs/devicons.lua
@@ -3,11 +3,25 @@ local M = {}
 local c = {
     white = '#cdd6f4',
     yellow = '#f9e2af',
+    gray = '#666666',
 }
 
 M = {
     color_icons = true,
     strict = true,
+
+    override_by_filename = {
+        ['.gitignore'] = {
+            icon = '',
+            color = c.gray,
+            name = 'Gitignore',
+        },
+        ['.bashrc'] = {
+            icon = '',
+            color = c.gray,
+            name = 'Bashrc',
+        },
+    },
 
     override_by_extension = {
         ['ml'] = {
@@ -16,7 +30,7 @@ M = {
             name = 'Ocaml',
         },
         ['txt'] = {
-            icon = '󰈙',
+            icon = '',
             color = c.white,
             name = 'Text',
         },
