1f191fbc1196fe15c66e6097ec4b44c289e6da4c
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2023-12-01 10:39:25 -0500

    Change deno to eslint_d in the linter and use more events for signaling a lint check

--- configs/lint.lua
+++ configs/lint.lua
@@ -1,12 +1,16 @@
 local lint = require 'lint'
 
 lint.linters_by_ft = {
-    javascript = { 'deno' },
-    typescript = { 'deno' },
+    javascript = { 'eslint_d' },
+    typescript = { 'eslint_d' },
     python = { 'mypy', 'ruff' },
+    lua = { 'luacheck' },
 }
 
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+lint.linters['luacheck'].args =
+    { '--ignore', 'vim', '--codes', '--ranges', '-' }
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'TextChanged' }, {
     callback = function()
         lint.try_lint()
     end,
